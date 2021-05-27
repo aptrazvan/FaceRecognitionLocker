ae7f16c51374969cf8226bc9605698443d9204fa
aptrazvan <aptrazvan@gmail.com>
2021-05-27 04:19:52 +0300

    Fixing pin input

--- From_ComputerNerd/From_ComputerNerd.ino
+++ From_ComputerNerd/From_ComputerNerd.ino
@@ -535,7 +535,7 @@ void arduinoUnoInut(void) {
   
     /* Setup the 8mhz PWM clock
   * This will be on pin 11*/
-  DDRB |= (1 << 1);//pin 9
+  DDRB &= ~(1 << 1);//pin 9
   DDRD |= (1 << 6);
   TCCR0A = (1 << COM0A0) | (1 << WGM01) | (1 << WGM00);
   TCCR0B = (1 << WGM02) | (1 << CS00);
